77cf5c817949fefd5321ee122f15d2190eaea6db
Yoonchul Chung <84674889+EricoFetcher@users.noreply.github.com>
2022-10-21 19:46:19 +0900

    submit

---
+++ 2022 ICPC Sinchon Summer Algorithm Camp Contest/25496/Ims.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int accumulatedTiredness = 0;
+int num_accessories = 0;
+int *tiredness = NULL;
+
+int main()
+{
+    scanf("%d %d", &accumulatedTiredness, &num_accessories);
+    tiredness = (int *)malloc(num_accessories * sizeof(int *));
+
+    for(int input_tiredness = 0; input_tiredness < num_accessories; input_tiredness++)
+    {
+        scanf("%d", tiredness + input_tiredness);
+
+    }
+
+    free(tiredness);
+    return 0;
+}
+
+void makeOrder()
+{
+    
+}
+
+void createAccessories(int * givenTiredness)
+{
+    accumulatedTiredness += *givenTiredness; 
+}
